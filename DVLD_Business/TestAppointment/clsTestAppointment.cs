9f4c01b472960e2ec89dfcd66211772c58afec1c
-l <abd@gmail.com>
2026-09-01 07:46:20 +0300

    Eding Bus and dataaccess for TestAppoiment

--- DVLD_Business/TestAppointment/clsTestAppointment.cs
+++ DVLD_Business/TestAppointment/clsTestAppointment.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DVLD_DataAccess;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -29,6 +30,64 @@ namespace DVLD_Business
 
         public int RetakeTestApplicationID { set; get; }
 
+        public clsTestAppointment()
+        {
+            this.TestAppointmentID = -1;
+            this.TestTypeID = -1;
+            this.LocalLicenseApplicationID = -1;
+            this.AppointmentDate = DateTime.Now;
+            this.PaidFees = -1;
+            this.CreateByUserID = -1;
+            this.IsLocked = false;
+            this.RetakeTestApplicationID = -1;
+        }
+
+        private clsTestAppointment(int TestAppointmentID, int TestTypeID,int LocalLicenseApplicationID
+            ,DateTime AppointmentDate,decimal PaidFees,int CreateByUserID, bool IsLocked, int RetakeTestApplicationID)
+        {
+            this.TestAppointmentID = TestAppointmentID;
+            this.TestTypeID = TestTypeID;
+            this.LocalLicenseApplicationID = LocalLicenseApplicationID;
+            this.AppointmentDate = AppointmentDate;
+            this.PaidFees = PaidFees;
+            this.CreateByUserID = CreateByUserID;
+            this.IsLocked = IsLocked;
+            this.RetakeTestApplicationID = RetakeTestApplicationID;
+        }
+
+        private bool _AddTestAppointment()
+        {
+            this.TestTypeID = clsTestAppointmentData.AddNewAppointment(TestTypeID, LocalLicenseApplicationID
+                , AppointmentDate, PaidFees, CreateByUserID, IsLocked, RetakeTestApplicationID);
+
+            return (this.TestAppointmentID != -1);
+        }
+
+        private bool _UpdateTestAppointment()
+        {
+            return clsTestAppointmentData.UpdateAppointment(this.TestAppointmentID, this.AppointmentDate);
+        }
+
+        public bool Save()
+        {
+            switch (Mode)
+            {
+                case enMode.Add:
+                    if(_AddTestAppointment())
+                    {
+                        Mode = enMode.Update;
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                case enMode.Update:
+                    return _UpdateTestAppointment();
+  
+            }
+            return false;
+        }
 
     }
 }
--- DVLD_DataAccess/TestAppointment/clsTestAppointmentData.cs
+++ DVLD_DataAccess/TestAppointment/clsTestAppointmentData.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Net;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +11,97 @@ namespace DVLD_DataAccess
 {
     public class clsTestAppointmentData
     {
+
+
+
+        public static int AddNewAppointment(int TestTypeID, int LocalLicenseApplicationID,DateTime AppointmentDate,
+            decimal PaidFees, int CreateByUserID, bool IsLocked, int RetakeTestApplicationID)
+        {
+            int TestAppointmentID = -1;
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string quary = @"INSERT INTO [dbo].[TestAppointments]
+                            ([TestTypeID],[LocalDrivingLicenseApplicationID],[AppointmentDate],[PaidFees],
+                            [CreatedByUserID],[IsLocked],[RetakeTestApplicationID])
+                            VALUES (@TestTypeID , @LocalLicenseApplicationID , @AppointmentDate,
+                            @PaidFees , @CreateByUserID , @IsLocked , @RetakeTestApplicationID;
+                            Select SCOPE_IDENTITY();";
+
+            SqlCommand command = new SqlCommand(quary, connection);
+
+            command.Parameters.AddWithValue("@TestTypeID", TestTypeID);
+            command.Parameters.AddWithValue("@LocalLicenseApplicationID", LocalLicenseApplicationID);
+            command.Parameters.AddWithValue("@AppointmentDate", AppointmentDate);
+            command.Parameters.AddWithValue("@PaidFees", PaidFees);
+            command.Parameters.AddWithValue("@CreateByUserID", CreateByUserID);
+            command.Parameters.AddWithValue("@IsLocked", IsLocked);
+
+            if (RetakeTestApplicationID != -1)
+            {
+                command.Parameters.AddWithValue("@RetakeTestApplicationID", RetakeTestApplicationID);
+            }
+            else
+            {
+                command.Parameters.AddWithValue("@RetakeTestApplicationID", System.DBNull.Value);
+            }
+
+            try
+            {
+                connection.Open();
+                
+                object result = command.ExecuteScalar();
+                
+                if (result != null && int.TryParse(result.ToString(), out int insertvalue))
+                {
+                    TestAppointmentID = insertvalue;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error" + e.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return TestAppointmentID;
+        }
+
+        public static bool UpdateAppointment(int TestAppointmentID ,DateTime AppointmentDate)
+        {
+            int RowAffectid = 0;
+
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string quary = @"UPDATE [dbo].[TestAppointments]
+                            SET [AppointmentDate] = @AppointmentDate
+                            WHERE TestAppointmentID = @TestAppointmentID";
+
+            SqlCommand command = new SqlCommand(quary, connection);
+
+            command.Parameters.AddWithValue("@TestAppointmentID", TestAppointmentID);
+            command.Parameters.AddWithValue("@AppointmentDate", AppointmentDate);
+
+            try
+            {
+                connection.Open();
+
+                RowAffectid = command.ExecuteNonQuery();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error " + e.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return (RowAffectid > 0);
+        }
 
     }
 }
